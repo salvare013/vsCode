25e1250a01f741c9325bdfe4bec9b21aa00ce284
salvare013 <salvare00013@foxmail.com>
2024-08-15 23:36:45 +0800

    commit update

---
+++ Window-main/c_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  printf(">>>");
+  char str[99];
+  scanf("%s", str);
+  printf(">>>%s\n", str);
+
+  return 0;
+}
---
+++ Window-main/cxx_test.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+int main(void) {
+  std::cout << ">>>";
+  std::string str;
+  std::cin >> str;
+  std::cout << ">>>" << str;
+
+  return 0;
+}
